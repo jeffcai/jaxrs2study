eaa964c7d13f67503fbe89d979a8bc246d34fd9f
JIAN FENG CAI <jfcai@cn.ibm.com>
2016-06-16 20:04:13 +0800

    added apache wink which seems out of date and no one is maintaining it anymore, may need to look at apache cxf

---
+++ apache-wink/src/main/java/com/jeffcaijf/jaxrs2/wink/MyResource.java
@@ -0,0 +1,30 @@
+package com.jeffcaijf.jaxrs2.wink;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by jeffcai on 6/16/2016.
+ */
+@Path("myresource")
+public class MyResource {
+
+    @Context
+    private HttpHeaders headers;
+
+    @Secured
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getIt() {
+        if (headers.getRequestHeader("basic") == null) {
+            return Response.status(Response.Status.FORBIDDEN).build();
+        }
+        return Response.ok("Got it!").build();
+    }
+
+}
